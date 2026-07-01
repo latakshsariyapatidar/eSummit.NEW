d8af7f5942b7748e1f550aba9692767574f19a45
latakshsariyapatidar <latakshpatidar@gmail.com>
2026-07-02 01:30:23 +0530

    fix: updated the api_base url

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export const API_BASE = "https://iic.iitdh.ac.in/esummit/api";
+export const API_BASE = "https://iic.iitdh.ac.in/esummit/api/api";
 
 export const FALLBACK_PASSES = [
   {
