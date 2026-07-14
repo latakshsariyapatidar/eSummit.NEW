f9ec67c23f0a6f6683f6f50c355a13e67718b097
latakshsariyapatidar <latakshpatidar@gmail.com>
2026-07-15 01:01:04 +0530

    feat: removed smooth scrolling

--- src/components/Shared/Layout.jsx
+++ src/components/Shared/Layout.jsx
@@ -1,17 +1,15 @@
 import { Nav } from "./Nav";
 import { Footer } from "./Footer";
-import { SmoothScroll } from "./SmoothScroll";
+
 
 export function Layout({ children }) {
   return (
     <>
       <Nav />
-      <SmoothScroll>
         <main className="min-h-screen bg-background relative z-10 shadow-2xl">
           {children}
         </main>
         <Footer />
-      </SmoothScroll>
     </>
   );
 }
